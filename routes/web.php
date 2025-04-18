4631029633dda3af33f988e0c023e8af4a05c001
Joao Patricio <ijpatricio@gmail.com>
2025-04-18 23:24:59 +0100

    Remove Inertia

--- routes/web.php
+++ routes/web.php
@@ -7,18 +7,9 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('welcome');
+    return 'Hello world';
 })->name('home');
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', DashboardController::class)->name('dashboard');
-
-    // Server routes
-    Route::resource('servers', ServerController::class);
-});
-
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
 
 Route::middleware('signed:relative')->group(function () {
     Route::get('/add-server-script/{server}', AddServerScriptController::class)->name('add-server-script');
