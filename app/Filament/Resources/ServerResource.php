3b99499c626637efe0ede254a9ff185b8e66fd46
Joao Patricio <ijpatricio@gmail.com>
2025-04-18 23:51:10 +0100

    Install Filament. Start Server resource.

---
+++ app/Filament/Resources/ServerResource.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Enums\ServerStatus;
+use App\Filament\Resources\ServerResource\Pages;
+use App\Filament\Resources\ServerResource\RelationManagers;
+use App\Models\Server;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class ServerResource extends Resource
+{
+    protected static ?string $model = Server::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Section::make('General Information')
+                    ->description('Server details')
+                    ->schema([
+                        Forms\Components\Grid::make()
+                            ->schema([
+                                Forms\Components\TextInput::make('name')
+                                    ->required()
+                                    ->maxLength(255),
+                                Forms\Components\TextInput::make('public_ipv4')
+                                    ->required()
+                                    ->maxLength(20),
+                                Forms\Components\TextInput::make('status')
+                                    ->hiddenOn('create')
+                                    ->maxLength(255)
+                                    ->default(ServerStatus::New),
+                                Forms\Components\TextInput::make('ssh_port')
+                                    ->numeric(),
+                            ])
+                            ->columns(),
+                ]),
+
+                Forms\Components\Section::make('Credentials')
+                    ->schema([
+                        Forms\Components\TextInput::make('username')
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('sudo_password')
+                            ->password()
+                            ->maxLength(255),
+                        Forms\Components\Toggle::make('provide_key')
+                            ->live()
+                            ->hiddenOn('edit'),
+                        Forms\Components\Textarea::make('private_key')
+                            ->visible(fn(Forms\Get $get) => $get('provide_key'))
+                            ->columnSpanFull(),
+                        Forms\Components\Textarea::make('public_key')
+                            ->visible(fn(Forms\Get $get) => $get('provide_key'))
+                            ->columnSpanFull(),
+                    ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('public_ipv4')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('status')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('username')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('ssh_port')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListServers::route('/'),
+            'create' => Pages\CreateServer::route('/create'),
+            'edit' => Pages\EditServer::route('/{record}/edit'),
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,10 @@
 <?php
 
 use App\Http\Controllers\AddServerScriptController;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\ServerController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return 'Hello world';
-})->name('home');
-
+Route::redirect('/', '/admin')->name('home');
 
 Route::middleware('signed:relative')->group(function () {
     Route::get('/add-server-script/{server}', AddServerScriptController::class)->name('add-server-script');
