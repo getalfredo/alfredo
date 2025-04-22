4e1cefb39b6d44a7d8d04250b78f2791d07c8996
Joao Patricio <ijpatricio@gmail.com>
2025-04-22 01:05:01 +0100

    Server Edit and View pages

--- app/Filament/Resources/ServerResource/Pages/EditServer.php
+++ app/Filament/Resources/ServerResource/Pages/EditServer.php
@@ -2,14 +2,10 @@
 
 namespace App\Filament\Resources\ServerResource\Pages;
 
-use App\Actions\CheckConnection;
-use App\Actions\InvitationLinkGenerator;
-use App\Filament\Resources\ServerResource;
-use Facades\App\Helpers\Script;
-use App\Models\Server;
 use Filament\Actions;
-use Filament\Notifications\Notification;
+use Facades\App\Helpers\Script;
 use Filament\Resources\Pages\EditRecord;
+use App\Filament\Resources\ServerResource;
 
 class EditServer extends EditRecord
 {
@@ -18,52 +14,9 @@ class EditServer extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\Action::make('generate_invitation')
-                ->label('Generate Invitation Link')
-                ->icon('heroicon-o-link')
-                ->outlined()
-                ->action($this->generateInvitation(...))
-                ->requiresConfirmation(),
-            Actions\Action::make('check_connection')
-                ->label('Check connection')
-                ->icon('heroicon-o-bolt')
-                ->outlined()
-                ->action($this->checkConnection(...)),
             Actions\ActionGroup::make([
                 Actions\DeleteAction::make(),
             ])
         ];
-    }
-
-    private function generateInvitation(InvitationLinkGenerator $linkGenerator): void
-    {
-        /** @var Server $server */
-        $server = $this->record;
-
-        $invitationURLScript = $linkGenerator->handle($server);
-
-        $command = Script::wrapCurl($invitationURLScript);
-
-        dd($command);
-    }
-
-    private function checkConnection(CheckConnection $checkConnection): void
-    {
-        /** @var Server $server */
-        $server = $this->record;
-
-        $isConnected = $checkConnection->handle($server);
-
-        if($isConnected) {
-            Notification::make()
-                ->title('Connection successful!')
-                ->color('success')
-                ->send();
-        } else {
-            Notification::make()
-                ->title('Oh no! Connection failed!')
-                ->color('danger')
-                ->send();
-        }
     }
 }
--- app/Filament/Resources/ServerResource/Pages/ViewServer.php
+++ app/Filament/Resources/ServerResource/Pages/ViewServer.php
@@ -2,11 +2,64 @@
 
 namespace App\Filament\Resources\ServerResource\Pages;
 
+use App\Actions\CheckConnection;
+use App\Actions\InvitationLinkGenerator;
 use App\Filament\Resources\ServerResource;
+use App\Models\Server;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewServer extends ViewRecord
 {
     protected static string $resource = ServerResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\Action::make('generate_invitation')
+                ->label('Generate Invitation Link')
+                ->icon('heroicon-o-link')
+                ->outlined()
+                ->action($this->generateInvitation(...))
+                ->requiresConfirmation(),
+            Actions\Action::make('check_connection')
+                ->label('Check connection')
+                ->icon('heroicon-o-bolt')
+                ->outlined()
+                ->action($this->checkConnection(...)),
+        ];
+    }
+
+    private function generateInvitation(InvitationLinkGenerator $linkGenerator): void
+    {
+        /** @var Server $server */
+        $server = $this->record;
+
+        $invitationURLScript = $linkGenerator->handle($server);
+
+        $command = Script::wrapCurl($invitationURLScript);
+
+        dd($command);
+    }
+
+    private function checkConnection(CheckConnection $checkConnection): void
+    {
+        /** @var Server $server */
+        $server = $this->record;
+
+        $isConnected = $checkConnection->handle($server);
+
+        if($isConnected) {
+            Notification::make()
+                ->title('Connection successful!')
+                ->color('success')
+                ->send();
+        } else {
+            Notification::make()
+                ->title('Oh no! Connection failed!')
+                ->color('danger')
+                ->send();
+        }
+    }
 }
